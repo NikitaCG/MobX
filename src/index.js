9f34e9ad28b254f384138278e3c5c2f353fc3767
Nikita Gorin <nikita.gorin@rt.ru>
2019-05-25 21:49:16 +0300

    babelrc config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DevTools from 'mobx-react-devtools';
+import { observable, action } from "mobx";
+import { observer } from 'mobx-react';
 
-class App extends React.Component {
+const nickName = observable({
+    firstName : 'nikita',
+    age: 23,
+
+    get nickName() {
+        console.log('work');
+        return `${this.firstName}${this.age}`;
+    },
+
+    increment() { this.age++ },
+
+    decrement() { this.age-- },
+},{
+    increment: action('plus'),
+    decrement: action('minus')
+},{
+    name:'observableObject'
+});
+
+const todos = observable([
+    { text: 'Learn React'},
+    { text: 'Learn MobX'}
+]);
+
+
+@observer class Counter extends React.Component {
+
+    handleIncrement = () => { this.props.store.increment() };
+    handleDecrement = () => { this.props.store.decrement() };
+
     render() {
         return (
             <div>
                 <DevTools />
-                <h1>Azaza mobX</h1>
+                <h1>{this.props.store.nickName}</h1>
+                <h1>{this.props.store.age}</h1>
+                <button onClick={this.handleDecrement}>-1</button>
+                <button onClick={this.handleIncrement}>+1</button>
+                <ul>
+                    {todos.map(({ text }) => <li key={text}>{text}</li>)}
+                </ul>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter store={nickName} />, document.getElementById('root'));
